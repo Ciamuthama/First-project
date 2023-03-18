685fba2e9b937181c898118bb164010fa3f5266b
Ciamuthama <muthamapeter79@gmail.com>
2023-03-18 23:42:09 +0300

    update

--- Projects/bonsai.js
+++ Projects/bonsai.js
@@ -1,20 +1,38 @@
-const cards = document.querySelector(".cards");
-const switchInputs = document.querySelectorAll(".switch-wrapper input");
-const prices = cards.querySelectorAll(".price1-hide");
-const toggleClass = "hide";
-for (const switchInput of switchInputs) {
-  switchInput.addEventListener("input", function () {
-    for (const price of prices) {
-      price.classList.add(toggleClass);
-    }
-    const activePrices = cards.querySelectorAll(
-      `.price1-hide.${switchInput.id}`
-    );
-    for (const activePrice of activePrices) {
-      activePrice.classList.remove(toggleClass);
-    }
-  });
+// function prices() {
+// 	const cards = document.querySelector(".cards");
+// 	const switchInputs = document.querySelectorAll(".switch-wrapper input");
+// 	const prices = cards.querySelectorAll(".price2-hide");
+	
+// 	const toggleClass = "hide";
+// 	for (const switchInput of switchInputs) {
+//   switchInput.addEventListener("input", function () {
+//     for (const price of prices) {
+//       price.classList.add(toggleClass);
+//     }
+//     const activePrices = cards.querySelectorAll(
+//       `.price2-hide.${switchInput.id}`
+//     );
+//     for (const Price of activePrices) {
+//       Price.classList.remove(toggleClass);
+//     }
+//   });
+// 	}
+// }
+
+let Monthly = document.querySelector(".price1-hide");
+let Yearly = document.querySelector('.price2-hide');
+
+document.querySelector('.switch-wrapper input').addEventListener('change', e => {
+  Monthly.style.display = e.target.checked ? 'block' : 'none';
+  Yearly.style.display = e.target.checked ? 'none' : 'block';
+})
+
+function Product() {
+  document.getElementsByClassName("mobile").classList.toggle("hide");
 }
+
+
+
 
 //Faq controls
 class DisclosureButton {
@@ -91,8 +109,65 @@ window.addEventListener(
 );
 
 
+function Mobile() {
+  document.getElementById("mobile-dropdown1").classList.toggle("show1");
+}
+
+window.onclick =function (event) {
+  if (!event.target.matches(".mobile-btn1")) {
+    var dropdowns = document.getElementsByClassName(".mobile-dropdown1");
+    var i;
+    for (i = 0; i <dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show1")) {
+        openDropdown.classList.remove('show1')
+      }
+      
+    }
+  }
+}
+
+function mobileNav() {
+  document.getElementById("mobile-container").classList.toggle("show-mobile-nav");
+}
+
+
+
+
+function Mobile2() {
+  document.getElementById("mobile-dropdown2").classList.toggle("show1");
+}
+
+window.onclick =function (event) {
+  if (!event.target.matches(".mobile-btn2")) {
+    var dropdowns = document.getElementsByClassName(".mobile-dropdown2");
+    var i;
+    for (i = 0; i <dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show1")) {
+        openDropdown.classList.remove('show1')
+      }
+      
+    }
+  }
+}
+
+function Click() {
+  document.getElementById("btn-nav").classList.toggle("active")
+}
+
+
+function Click2() {
+	document.getElementById("btn-nav2").classList.toggle("active")
+}
+
+function Btn() {
+	document.getElementById("btn1").classList.toggle("list3-active")
+}
+
 function Comparison() {
   document.getElementById("w-dropdown-list3").classList.toggle("show");
+ 
 }
 
 window.onclick =function (event) {
@@ -108,3 +183,24 @@ window.onclick =function (event) {
     }
   }
 }
+
+
+const mainNav = document.querySelector(".main-nav");
+const navigation = document.querySelector(".navigation2");
+
+const options = {
+  rootMargin:"-100px 0px 0px 0px"
+};
+
+const changeNav = new IntersectionObserver
+  (function (entries) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      mainNav.classList.add("scrolled")
+    } else {
+      
+    }
+  })
+}, options )
+
+changeNav.observe(navigation);
